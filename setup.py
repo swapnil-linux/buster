a3acec57f5f65e19c9b80ceed8d45b3d1df1e2ef
raccoony <raccoonyy@gmail.com>
2017-01-04 13:59:08 +0900

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup
 from buster import _version
 
 
-setup(name="buster",
-      version=_version.__version__,
-      description="Static site generator for Ghost and Github",
-      long_description=open("README.rst").read(),
-      author="Akshit Khurana",
-      author_email="axitkhurana@gmail.com",
-      url="https://github.com/axitkhurana/buster",
-      license="MIT",
-      packages=["buster"],
-      entry_points={"console_scripts": ["buster = buster.buster:main"]},
-      install_requires=['GitPython==0.3.2.RC1', 'async==0.6.1', 'docopt==0.6.1', 'gitdb==0.5.4', 'pyquery==1.2.8', 'smmap==0.8.2']
-    )
+setup(
+    name="buster",
+    version=_version.__version__,
+    description="Static site generator for Ghost and Github",
+    long_description=open("README.rst").read(),
+    author="Akshit Khurana",
+    author_email="axitkhurana@gmail.com",
+    url="https://github.com/axitkhurana/buster",
+    license="MIT",
+    packages=["buster"],
+    entry_points={"console_scripts": ["buster = buster.buster:main"]},
+    install_requires=[
+        'GitPython==2.1.1',
+        'async==0.6.2',
+        'docopt==0.6.1',
+        'gitdb==2.2.0',
+        'pyquery==1.2.17',
+        'smmap==2.1.1',
+        'pyquery==1.2.8',
+    ]
+)
