c3ea1170f8d4bdbd1b191fa123e0855a3306d529
Ramith Hettiarachchi <ramithuh@live.com>
2018-12-23 09:50:38 +0530

    no message

--- buster/buster.py
+++ buster/buster.py
@@ -186,7 +186,7 @@ def main():
             #modified_text = text.replace('http://blog.ramith.fyi', 'https://blog.ramith.fyi/static')
             #modified_text = modified_text.replace('localhost:2374', 'https://blog.ramith.fyi/static')
 
-            modified_text = text.replace('localhost:2368',github_url)
+            modified_text = text.replace('localhost:2368',github_url) #you might need to do changes as above.
 
             modified_text = modified_text.replace('pngg','png')
             modified_text = modified_text.replace('pngng','png')
